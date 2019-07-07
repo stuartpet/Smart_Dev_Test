245ef3ad6c598308762210d08aab816d87fba08f
Stuart <stuart.pethurst@gmail.com>
2019-07-07 15:36:01 +0100

    added modules for code used in multipal methods to make code more readable and future maintenance easier

--- lib/parse.rb
+++ lib/parse.rb
@@ -1,5 +1,18 @@
 # frozen_string_literal: true
+module Counter
+  def count(variable)
+    (variable).each_with_object(Hash.new(0)) { |o, h| h[o] += 1 }
+  end
+end
+
+module SortDescending
+  def descending_order(variable)
+    (variable).sort_by { |webpage, views| views }.reverse
+  end
+end
+
 class Parse
+  include Counter, SortDescending
   def initialize
     @file = []
     @webpage_views = []
@@ -13,13 +26,13 @@ class Parse
 
   def views_of_pages
     @webpage_views = @file.sort.map { |page| page[0] }
-    @webpage_views = @webpage_views.each_with_object(Hash.new(0)) { |o, h| h[o] += 1 }
-    @webpage_views = @webpage_views.sort_by { |webpage, views| views }.reverse
+    @webpage_views = count(@webpage_views)
+    @webpage_views = descending_order(@webpage_views)
   end
 
   def unique_page_visits
     @unique_views = @file.uniq.map { |page| page[0] }
-    @unique_views = @unique_views.each_with_object(Hash.new(0)) { |o, h| h[o] += 1 }
-    @unique_views = @unique_views.sort_by { |webpage, views| views }.reverse
+    @unique_views = count(@unique_views)
+    @unique_views = descending_order(@unique_views)
   end
 end
