133d2c90451d1cbde5241bbaa7351f0d8011b2d7
Stuart <stuart.pethurst@gmail.com>
2019-07-07 15:01:21 +0100

    added method to show unique page visits and replced mock test with real data

--- lib/parse.rb
+++ lib/parse.rb
@@ -14,9 +14,12 @@ class Parse
   def views_of_pages
     @webpage_views = @file.sort.map { |page| page[0] }
     @webpage_views = @webpage_views.each_with_object(Hash.new(0)) { |o, h| h[o] += 1 }
-    @webpage_views = @webpage_views.sort_by { |_k, v| v }.reverse
+    @webpage_views = @webpage_views.sort_by { |webpage, views| views }.reverse
   end
 
   def unique_page_visits
+    @unique_views = @file.uniq.map { |page| page[0] }
+    @unique_views = @unique_views.each_with_object(Hash.new(0)) { |o, h| h[o] += 1 }
+    @unique_views = @unique_views.sort_by { |webpage, views| views }.reverse
   end
 end
--- spec/parse_spec.rb
+++ spec/parse_spec.rb
@@ -12,15 +12,15 @@ describe Parse do
   describe 'views_of_pages' do
     it 'lists webpages with most page views from most page views to least' do
       subject.parse_file('webserver.log')
-      expect(subject.views_of_pages).to eq([['/about/2', 90], ['/contact', 89], ['/index', 82], ['/about', 81], ['/help_page/1', 80], ['/home',
-                                                                                                                                   78]])
+      expect(subject.views_of_pages).to eq([['/about/2', 90], ['/contact', 89], ['/index', 82], ['/about', 81], ['/help_page/1', 80], ['/home', 78]])
+
     end
   end
 
   describe 'unique_page_visits' do
     it 'lists webpages with most unique page views from most to least' do
-      unique_page_visits = double('unique_page_visits')
-      allow(unique_page_visits).to receive(:unique_page_visits).and_return("/home 8 unique visits")
+      subject.parse_file('webserver.log')
+      expect(subject.unique_page_visits).to eq([["/index", 23], ["/home", 23], ["/contact", 23], ["/help_page/1", 23], ["/about/2", 22], ["/about", 21]])
     end
   end
 end
