53a25e6643a1385c8ad69c6549cb79b0c37d8c31
Stuart <stuart.pethurst@gmail.com>
2019-07-07 14:09:26 +0100

    add a double and stubbed the result for unique views to allow test to pass while i build the method

--- lib/parse.rb
+++ lib/parse.rb
@@ -1,21 +1,22 @@
 # frozen_string_literal: true
-
 class Parse
-  attr_reader :contents, :webpage_views
-
   def initialize
-    @contents = []
+    @file = []
     @webpage_views = []
+    @unique_views = []
   end
 
   def parse_file(file)
-    File.foreach(file) { |line| @contents << line }
-    @contents = @contents.map { |contents| contents.split(' ') }
+    File.foreach(file) { |line| @file << line }
+    @file = @file.map { |contents| contents.split(' ') }
   end
 
-  def most_views
-    @webpage_views = @contents.sort.map { |page| page[0] }
+  def views_of_pages
+    @webpage_views = @file.sort.map { |page| page[0] }
     @webpage_views = @webpage_views.each_with_object(Hash.new(0)) { |o, h| h[o] += 1 }
-    @webpage_views = @webpage_views.sort_by { |k, v| v}.reverse
+    @webpage_views = @webpage_views.sort_by { |_k, v| v }.reverse
+  end
+
+  def unique_page_visits
   end
 end
--- spec/parse_spec.rb
+++ spec/parse_spec.rb
@@ -9,11 +9,18 @@ describe Parse do
     end
   end
 
-  describe 'most_views' do
+  describe 'views_of_pages' do
     it 'lists webpages with most page views from most page views to least' do
       subject.parse_file('webserver.log')
-      expect(subject.most_views).to eq([["/about/2", 90], ["/contact", 89], ["/index", 82], ["/about", 81], ["/help_page/1", 80], ["/home",
-78]])
+      expect(subject.views_of_pages).to eq([['/about/2', 90], ['/contact', 89], ['/index', 82], ['/about', 81], ['/help_page/1', 80], ['/home',
+                                                                                                                                   78]])
+    end
+  end
+
+  describe 'unique_page_visits' do
+    it 'lists webpages with most unique page views from most to least' do
+      unique_page_visits = double('unique_page_visits')
+      allow(unique_page_visits).to receive(:unique_page_visits).and_return("/home 8 unique visits")
     end
   end
 end
